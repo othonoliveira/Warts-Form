1b24eaaca486189b6dce34d6fd6105f29c7e54f5
Othon Oliveira <othonoliveira08@gmail.com>
2022-08-15 18:12:46 -0300

    Finalização dos requísitos

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const butaoEntrar = document.getElementById('butaoEntrar');
 const email = document.getElementById('email');
 const password = document.getElementById('senha');
+const disableCheckBox = document.getElementById('agreement');
+const submitButton = document.getElementById('submit-btn');
+const textArea = document.querySelector('#textarea');
+const counter = document.getElementById('counter');
+const form = document.getElementById('evaluation-form');
+const formData = document.getElementById('form-data');
+const nome = document.getElementById('input-name');
+const sobreNome = document.getElementById('input-lastname');
+const emailForm = document.getElementById('input-email');
+const casa = document.getElementById('house');
+const familia = document.getElementsByClassName('radio-family');
+const subject = document.getElementsByClassName('subject');
+const avaliation = document.getElementsByClassName('radio-rate');
+const observacao = document.getElementById('textarea');
 
 butaoEntrar.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -11,10 +25,7 @@ butaoEntrar.addEventListener('click', () => {
 });
 
 window.onload = function disableButton() {
-  const disableCheckBox = document.getElementById('agreement');
   // Retrieve reference to button
-  const submitButton = document.getElementById('submit-btn');
-
   disableCheckBox.addEventListener('change', (e) => {
     if (e.target.checked) {
       submitButton.disabled = false;
@@ -24,30 +35,43 @@ window.onload = function disableButton() {
   });
 };
 
-const textArea = document.querySelector('#textarea');
-const counter = document.getElementById('counter');
+textArea.addEventListener('keyup', (e) => {
+  counter.innerText = 500 - e.target.value.length;
+});
 
-// textArea.addEventListener('keydown', (e) => {
-//   contadorEmNumero = parseInt(counter.innerText);
+function insert(pai, conteudo) {
+  const novaDiv = document.createElement('div');
+  novaDiv.innerHTML = conteudo;
+  pai.append(novaDiv);
+}
 
-//   if (contadorEmNumero > 0 && contadorEmNumero <= 500){
-//         if(!(e.key == 'Backspace')){
-//             contadorEmNumero = contadorEmNumero - 1;
-//             counter.innerText = contadorEmNumero;
-//         } else if(e.key == 'Backspace' && contadorEmNumero != 500){
-//             contadorEmNumero = contadorEmNumero + 1;
-//             counter.innerText = contadorEmNumero;
-//         }
-//     } else if (contadorEmNumero == 0 && e.key == 'Backspace') {
-//         contadorEmNumero = contadorEmNumero + 1;
-//         counter.innerText = contadorEmNumero;
-//     }
-// });
+function checkedRadio(radios) {
+  for (let index = 0; index < radios.length; index += 1) {
+    if (radios[index].checked) {
+      return radios[index].value;
+    }
+  }
+}
 
-textArea.addEventListener('keyup', () => {
-  // const caracteresArea = textArea.value;
-  let r;
-  r = 500;
-  r = 500 - (document.querySelector('#textarea').value.length);
-  counter.innerText = r;
+function checkedBox(checkBox) {
+  const arr = [];
+  for (let index = 0; index < checkBox.length; index += 1) {
+    if (checkBox[index].checked) {
+      arr.push(checkBox[index].value);
+    }
+  }
+  return arr.join(', ');
+}
+
+const sendButton = document.getElementById('submit-btn');
+sendButton.addEventListener('click', (event) => {
+  insert(formData, `Nome: ${nome.value} ${sobreNome.value}`);
+  insert(formData, `Email: ${emailForm.value}`);
+  insert(formData, `Casa: ${casa.value}`);
+  insert(formData, `Família: ${checkedRadio(familia)}`);
+  insert(formData, `Matérias: ${checkedBox(subject)}`);
+  insert(formData, `Avaliação: ${checkedRadio(avaliation)}`);
+  insert(formData, `Observações: ${observacao.value}`);
+  form.style.display = 'none';
+  event.preventDefault();
 });
